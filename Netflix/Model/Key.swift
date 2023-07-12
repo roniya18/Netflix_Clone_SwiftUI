ac1f6aaff4a8fee105d917cd7e79517b434a43c2
roniya18 <121227793+roniya18@users.noreply.github.com>
2023-07-12 10:56:35 +0530

    Update Key.swift

--- Netflix/Model/Key.swift
+++ Netflix/Model/Key.swift
@@ -7,5 +7,5 @@
 
 import SwiftUI
 
-let APIKey = "ac1cb11b4404f06868e18615217e87fd"
-let APIReadAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzFjYjExYjQ0MDRmMDY4NjhlMTg2MTUyMTdlODdmZCIsInN1YiI6IjY0YTI3ODdiODI4OWEwMDBjYWYwYjBjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PvvvTG6uBGdZGgydRdmbzRQk3-MjHujH87xfwetERm0"
+let APIKey = ""
+let APIReadAccessToken = ""
